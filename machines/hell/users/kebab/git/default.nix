0c4834df92b4ffb0229e74900862dd0eeaa8c69c
KebabObama <lucaschyba@gmail.com>
2026-03-07 23:27:28 +0100

    I have succeded

--- machines/hell/users/kebab/git/default.nix
+++ machines/hell/users/kebab/git/default.nix
@@ -18,7 +18,14 @@
       url."https://github.com/".insteadOf = "github:";
       user.name = "KebabaObama";
       user.email = "lucaschyba@gmail.com";
-      credential.helper = "!f() { if [ \"$1\" = \"get\" ]; then echo \"username=x-access-token\"; echo \"password=$(cat ${config.sops.secrets.github-token.path})\"; fi; }; f";
+      credential.helper = ''
+        !f() {
+          if [ "$1" = "get" ]; then
+            echo "username=x-access-token"
+            echo "password=$(cat ${config.sops.secrets.github-token.path})"
+          fi
+        }; f
+      '';
     };
   };
 }
